fcd7d121fa24f364f37339be3cd171daa3797d8e
Pedro Santos <pedrosino@gmail.com>
2016-09-02 17:23:21 -0300

    Now with log in to google.

--- google-group_spider.py
+++ google-group_spider.py
@@ -3,17 +3,17 @@ import scrapy
 from scrapy.utils.url import escape_ajax
 
 class StackOverflowSpider(scrapy.Spider):
-    name = 'google-group'
-    start_urls = ["https://groups.google.com/forum/#!forum/django-cs"]
+  name = 'google-group'
+  start_urls = ["https://groups.google.com/forum/#!forum/django-cs"]
 
-    def parse(self, response):
-        for href in response.css('a::attr(href)'):
-            full_url = response.urljoin(href.extract())
-            yield scrapy.Request(full_url, callback=self.parse_question)
+  def parse(self, response):
+    for href in response.css('a::attr(href)'):
+      full_url = response.urljoin(href.extract())
+      yield scrapy.Request(full_url, callback=self.parse_question)
 
-    def parse_question(self, response):
-        yield {
-            'date': response.xpath('//td[@class="lastPostDate"]//text()').extract_first(),
-            'link': response.url,
-            'title': response.xpath('//h2//text()').extract()
-        }
+  def parse_question(self, response):
+    yield {
+      'date': response.xpath('//td[@class="lastPostDate"]//text()').extract_first(),
+      'link': response.url,
+      'title': response.xpath('//h2//text()').extract()
+    }
---
+++ login_spyder.py
@@ -0,0 +1,40 @@
+import scrapy
+
+from scrapy.utils.url import escape_ajax
+
+class StackOverflowSpider(scrapy.Spider):
+  name = 'login'
+  start_urls = ["https://accounts.google.com/ServiceLogin?hl=pt-BR#identifier"]
+
+  def parse(self, response):
+    """
+    Insert the email. Next, go to the password page.
+    """
+    return scrapy.FormRequest.from_response(
+      response,
+      formdata={'Email': 'blah'},
+      callback=self.log_password)
+
+  def log_password(self, response):
+    """
+    Enter the password to complete the log in.
+    """
+    return scrapy.FormRequest.from_response(
+      response,
+      formdata={'Passwd': 'blah'},
+      callback=self.after_login)
+
+  def after_login(self, response):
+    return scrapy.Request("https://groups.google.com/forum/?_escaped_fragment_=forum/wca-delegates[1-100]", callback=self.parse_forum)
+
+  def parse_forum(self, response):
+    for href in response.css('a::attr(href)'):
+      full_url = response.urljoin(href.extract())
+      yield scrapy.Request(full_url, callback=self.parse_thread)
+
+  def parse_thread(self, response):
+    yield {
+      'date': response.xpath('//td[@class="lastPostDate"]//text()').extract_first(),
+      'link': response.url,
+      'title': response.xpath('//h2//text()').extract()
+    }
